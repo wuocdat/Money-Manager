0e2c3f65cb6309ffcadc52fb35323fe9a0887437
wuocdat <quocdatpadpqn@gmail.com>
2023-12-28 20:01:00 +0700

    set blur red to progress track when alert occur

--- app/src/main/java/com/wuocdat/moneymanager/View/Fragment/OverviewFragment.kt
+++ app/src/main/java/com/wuocdat/moneymanager/View/Fragment/OverviewFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.progressindicator.LinearProgressIndicator
@@ -90,6 +91,8 @@ class OverviewFragment : Fragment() {
                     percentTextView.text = getString(R.string.percent_text, progressPercent)
                     if (progressPercent > 90) {
                         progress.setIndicatorColor(Color.RED)
+                        progress.trackColor =
+                            ContextCompat.getColor(requireContext(), R.color.blur_red)
                         percentTextView.setTextColor(Color.RED)
                     }
                 } else {
